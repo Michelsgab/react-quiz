d31d59ccaf01c38498e2dd29d75834740ce3c65b
Michelsgab <eugabmichels@gmail.com>
2022-06-18 16:20:38 -0300

    Question Card Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import QuestionCards from "./components/QuestionCard";
 
 function App() {
-  return <div className="App">Quiz</div>;
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>React Quiz About Vehicles</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCards />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function QuestionCards() {
+  return <div>Hello my friend</div>;
+}
